e7cef3be41d23e4f3d0ff2a2b710a8d4384f1183
Paul Ashby <paul@primitive.co>
2023-05-19 12:26:49 +0100

    Add carousel to project page

--- src/templates/project.php
+++ src/templates/project.php
@@ -1,3 +1,18 @@
 <?php namespace ProcessWire;
 
-/* Project template */
+$components = $page->project_component;
+$component_markup = [];
+
+foreach ($components as $component) {
+	$component_type = $component->component_type->name;
+
+    if ($component_type === "carousel") {
+		$component_markup[] = $files->render("components/carousel/index.php", ["slides" => $component->carousel]);
+	}
+}
+
+?>
+
+<main data-pw-id="main-region">
+	<?= implode($component_markup) ?>
+</main>
