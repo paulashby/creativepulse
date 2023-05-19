a640b53b2989e9655e0d189464dff7cb047347a3
Paul Ashby <paul@primitive.co>
2023-05-19 17:53:46 +0100

    Refactor carousel js

--- src/templates/components/carousel/script.js
+++ src/templates/components/carousel/script.js
@@ -1,44 +1,31 @@
 
+const state = {
+  currSlide: 0,
+  direction: 1
+};
 
-function init() {
-  // This is a static node list - test what happens when there's more that one - we may need to do each individually using an id
+let carouselElmt;
+let slider;
+let slideSet;
+let slideSetClone;
+let slideCount;
+let carouselControls;
+let debounce = false;
 
-  const carouselElmt = document.querySelector(".carousel");
-  const slider = carouselElmt.querySelector(".carousel-slider");
-  const slideSet = slider.querySelector(".carousel-slider__set");
-  const slideCount = slideSet.children.length;
-
-  const state = {
-    hasTransformed: false,
-    currSlide: 0,
-    adjustment: 0
-  };
+const init = () => {
+  carouselElmt = document.querySelector(".carousel");
+  slider = carouselElmt.querySelector(".carousel-slider");
+  slideSet = slider.querySelector(".carousel-slider__set");
+  slideSetClone = slideSet.cloneNode(true);
+  slideCount = slideSet.children.length;
+  carouselControls = carouselElmt.querySelector(".carousel-controls");
   
-  const updateState = () => {
-    state.slideW = slideSet.children[0].offsetWidth;
-    state.slideSetW = state.slideW * slideCount;
-    state.slideVW = state.slideW / window.innerWidth * 100;
-    state.slideSetVW = state.slideSetW / window.innerWidth * 100;
-  };
-
-  updateState();
-  
-  // Double up slides to allow scrolling
-  const slideSetClone = slideSet.cloneNode(true);
-  slider.append(slideSetClone);
-  
-  const carouselControls = carouselElmt.querySelector(".carousel-controls");
-  
-  let debounce = false;
-
-  carouselControls.addEventListener("click", (e) => {
-    updateSlider(e);
-  });
-
   window.addEventListener("resize", () => {
-
     if (!debounce) {
+      // Store new widths
       updateState();
+      
+      // Use new widths
       updateSlider();
 
       debounce = true;
@@ -46,35 +33,49 @@ function init() {
     }
   });
 
-  const updateSlider = (e = false) => {
+  carouselControls.addEventListener("click", (e) => {
+    updateSlider(e);
+  });
 
-    if (e) {
-      const action = e.target.dataset.action;
-      const adjustment = action === "prev" ? 1 : -1;
+  slider.append(slideSetClone);
 
-      if (Math.abs(state.currSlide) % slideCount === 0) {
-        // Don't adjust before first transform
-        if (adjustment === 1 || state.hasTransformed) {
-          state.currSlide -= adjustment * slideCount;
-          resetSliderPosition();          
-        }
-        state.hasTransformed = true;
+  updateState();
+}
+
+const updateState = () => {
+  state.slideW = slideSet.children[0].offsetWidth;
+  state.slideSetW = state.slideW * slideCount;
+  state.slideVW = state.slideW / window.innerWidth * 100;
+  state.slideSetVW = state.slideSetW / window.innerWidth * 100;
+};
+
+const updateSlider = (e = false) => {
+  if (e) {
+    const action = e.target.dataset.action;
+    const adjustment = action === "prev" ? 1 : -1;
+
+    if (Math.abs(state.currSlide) % slideCount === 0) {
+      // Don't reset when changing direction
+      if (adjustment === state.direction) {
+        state.currSlide -= adjustment * slideCount;
+        resetSliderPosition();          
       }
-      
-      state.currSlide += adjustment;
-      slider.style.transform = `translateX(${(state.currSlide * state.slideVW)}vw)`;
-    } else {
-      resetSliderPosition();
     }
-  };
-
-  // Position slider so slides can seamlessly loop
-  const resetSliderPosition = () => {
-    slider.classList.add("transition-off");
+    state.currSlide += adjustment;
+    state.direction = adjustment;
     slider.style.transform = `translateX(${(state.currSlide * state.slideVW)}vw)`;
-    slider.offsetHeight; // Trigger layout to perform translation with transtion disabled
-    slider.classList.remove("transition-off");
+  } else {
+    // Window resize
+    resetSliderPosition();
   }
+};
+
+// Position slider so slides loop
+const resetSliderPosition = () => {
+  slider.classList.add("transition-off");
+  slider.style.transform = `translateX(${(state.currSlide * state.slideVW)}vw)`;
+  slider.offsetHeight; // Trigger layout to perform translation with transtion disabled
+  slider.classList.remove("transition-off");
 }
 
 export const carousel = {
