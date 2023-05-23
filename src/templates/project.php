3157ede5034cf6225ed8fb35712608de22f7a5bc
Paul Ashby <paul@primitive.co>
2023-05-23 09:41:06 +0100

    Consolidate component markup and index files

--- src/templates/project.php
+++ src/templates/project.php
@@ -7,7 +7,7 @@ foreach ($components as $component) {
 	$component_type = $component->component_type->name;
 
     if ($component_type === "carousel") {
-		$component_markup[] = $files->render("components/carousel/index.php", ["slides" => $component->carousel]);
+		$component_markup[] = $files->render("components/carousel/index.php", ["slide_images" => $component->carousel]);
 	}
 }
 
