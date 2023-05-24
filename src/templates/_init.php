9286eece15bcf76c80533cd21586d076ecc15173
Paul Ashby <paul@primitive.co>
2023-05-24 12:13:50 +0100

    Move variables to _init.php

--- src/templates/_init.php
+++ src/templates/_init.php
@@ -1,6 +1,12 @@
 <?php namespace ProcessWire;
 
-// Optional initialization file, called before rendering any template file.
-// This is defined by $config->prependTemplateFile in /site/config.php.
-// Use this to define shared variables, functions, classes, includes, etc. 
+/** @var Page $page */
+/** @var Pages $pages */
+/** @var Config $config */
 
+$title = $page->title;
+$home = $pages->get('/');
+$top_level_pages = $home->children;
+$template_path = $config->urls->templates;
+$js_url = $template_path . glob( "js/main.min.*.js" )[0];
+$css_url = $template_path . glob( "css/main.min.*.css" )[0];
--- src/templates/_main.php
+++ src/templates/_main.php
@@ -1,15 +1,6 @@
 <?php namespace ProcessWire;
 
-/** @var Page $page */
-/** @var Pages $pages */
-/** @var Config $config */
-
-$title = $page->title;
-$home = $pages->get('/');
-$top_level_pages = $home->children;
-$template_path = $config->urls->templates;
-$js_url = $template_path . glob( "js/main.min.*.js" )[0];
-$css_url = $template_path . glob( "css/main.min.*.css" )[0];
+// Variables declared in _init.php
 
 ?>
 
