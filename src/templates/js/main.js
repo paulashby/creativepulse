05b10580458847fba8140cc33f6542aabb55844a
Paul Ashby <paul@primitive.co>
2023-05-28 16:51:00 +0100

    Add helpers to provide component widths and margins

--- src/templates/js/main.js
+++ src/templates/js/main.js
@@ -2,7 +2,24 @@ import { nav } from "../components/nav/script";
 import { footer } from "../components/footer/script";
 import { carousel } from "../components/carousel/script";
 
+const debouncedResizeEvent = new Event("debouncedResize");
+const body = document.querySelector("body");
+
+let debounce;
+
 window.addEventListener("DOMContentLoaded", (event) => {
+
+    window.addEventListener("resize", () => {   
+
+        clearTimeout(debounce);
+        body.classList.add("resizing");
+        
+        debounce = setTimeout(() => {            
+           body.classList.remove("resizing");
+           window.dispatchEvent(debouncedResizeEvent);
+        }, 250);
+    });
+
     nav.init();
     carousel.init();
 });
--- src/templates/project.php
+++ src/templates/project.php
@@ -8,8 +8,27 @@ $component_markup = [];
 foreach ($components as $component) {
 	$component_type = $component->component_type->name;
 
-    if ($component_type === "carousel") {
-		$component_markup[] = $files->render("components/carousel/index.php", ["slide_images" => $component->carousel]);
+    // Don't use component_width for carousel - these are always page-width
+    $component_width = $component_type === "carousel" ? "page-width" : $component->component_width->name;
+
+
+    // Just testing a couple - ultimately we'll just output all without checking the type
+    // if ($component_type === "testimonial") {
+    //     $component_markup[] = "<div class='component component--$component_width'>";
+    //     $component_markup[] = $files->render("components/testimonial/index.php");
+    //     $component_markup[] = "</div>";
+    // }
+
+    if ($component_type === "carousel") {        
+        $component_markup[] = renderComponent($component_type, $component_width, $vars=["slide_images" => $component->carousel]);
+	}
+    if ($component_type === "testimonial") {  
+        $component_markup[] = renderComponent($component_type, $component_width);      
+        $component_markup[] = renderComponent($component_type, "content-half-width");
+        $component_markup[] = renderComponent($component_type, "content-width");
+        $component_markup[] = renderComponent($component_type, "page-width");
+
+        
 	}
 }
 
