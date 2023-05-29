a3c124e41b6fa8d51a2a9d12bafb2aed4ea31b09
Paul Ashby <paul@primitive.co>
2023-05-29 09:10:58 +0100

    Reorder slides to allow for slider offset

--- src/templates/components/carousel/index.php
+++ src/templates/components/carousel/index.php
@@ -4,6 +4,10 @@ $slides = "";
 $indicators = "";
 $count = 0;
 
+// Slider is shifted so first slide 'bleeds off'. Reorder slides so left-most slide is last one in the given set.
+$left_most_image = $slide_images->pop();
+$slide_images->prepend($left_most_image);
+
 foreach ($slide_images as $slide) {
     $slides .= "<div class='carousel-slide'>
         <img src='{$slide->url()}' alt='{$slide->description}'>
