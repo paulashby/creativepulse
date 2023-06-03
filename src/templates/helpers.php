c884aad601d5b6ad8ffa216c1e745c9cd820459e
Paul Ashby <paul@primitive.co>
2023-06-03 15:45:15 +0100

    Adjust content div for Firefox

--- src/templates/helpers.php
+++ src/templates/helpers.php
@@ -3,7 +3,6 @@
 // Render component of the given type in wrapper div with class based on given width
 function renderComponent($type, $width, $vars = []) {
     $component_inner = wire("files")->render("components/$type/index.php", $vars);
-bd($type);
     return "<div class='$type component component--$width'>
         $component_inner
     </div>";
