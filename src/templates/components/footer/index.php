0a81cf274f10a47f4805cecc3aba86e1bd2e11d5
Paul Ashby <paul@primitive.co>
2023-06-09 19:00:24 +0100

    Initial footer styling

--- src/templates/components/footer/index.php
+++ src/templates/components/footer/index.php
@@ -1,8 +1,35 @@
 <footer>
-    <h2>Have a project in mind?</h2>
-    <p>Please call, email, or fill in this form to let us know what you’re looking for.</p>
-    <form action="">
-        <label for="fname" class="form-label sr-only">First name</label>
-        <input type="text" id="fname" name="fname" placeholder="First name">
+    <div class="footer-text">
+        <h2>Have a project in mind?</h2>
+        <p>Please call, email, or fill in this form to let us know what you&apos;re looking for.</p>
+    </div>
+    <form class="contact-form" action="">
+        <fieldset class="details">
+            <label for="fname" class="form-label sr-only">First name</label>
+            <input type="text" id="fname" name="fname" placeholder="First name">
+            <label for="lname" class="form-label sr-only">Last name</label>
+            <input type="text" id="lname" name="lname" placeholder="Last name">
+            <label for="email" class="form-label sr-only">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email">
+        </fieldset>
+        <label for="message" class="form-label sr-only">Message</label>
+        <textarea type="text" id="message" name="message" placeholder="Message"></textarea>
+            <input type="submit" value="Send" />
+            <input type="checkbox" id="consent" name="consent" value="consent">
+            <label for="consent">I consent to this website storing my information while processing my enquiry</label>
+        </fieldset>
     </form>
+    <address>
+        <p class="postal">
+            The Creative Pulse</ br>
+            2 West Street,</ br>
+            Epsom,</ br>
+            Surrey KT18 7RG
+        </p>
+        <div class="contact"></div>
+            <p class="email">info@thecreativepulse.com</p>
+            <p class="tel">0800 112 3228</p> 
+        <div>
+    </address>
+    <p class="copyright">&copy; 2023 The Creative Pulse</p>
 </footer>
