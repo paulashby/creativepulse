e17b07e3e1c2c56dfeef070a41ce1561c8443610
Paul Ashby <paul@primitive.co>
2023-08-07 11:23:26 +0100

    Add project styles up to large screen width

--- src/templates/components/text-section/index.php
+++ src/templates/components/text-section/index.php
@@ -7,7 +7,9 @@ $text = $component->text;
 
 <?php if ($heading) : ?>
     <div class="component-content">
-        <h2 class="subheading"><?= $heading ?></h2>
+        <div class="subheading">
+            <h2><?= $heading ?></h2>
+        </div>
 <?php else : ?>
     <div class="component-content untitled">
 <?php endif; ?>
