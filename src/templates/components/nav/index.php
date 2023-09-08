189ae5b8027d9a02fe286a5944a424a99939d70e
Paul Ashby <paul@primitive.co>
2023-09-08 18:49:05 +0100

    Remove sticky wrapper from nav element

--- src/templates/components/nav/index.php
+++ src/templates/components/nav/index.php
@@ -4,23 +4,21 @@ $nav_entries = $entries->implode("\n", "<li class='nav__submenu-entries-list-lin
 
 ?>
 
-<div class="nav-sticky-wrapper">
-    <nav class="nav">
-        <ul class="nav__elements">
-            <li class="nav__element nav__home"><a href="/" class="nav__home-link"><span class="sr-only">Home</span></a></li>
-            <li class="nav__element nav__submenu">
-                <ul>
-                    <li class="nav__submenu-toggle">
-                        <div class="nav__submenu-toggle-bar"></div>
-                    </li>
-                    <li class="nav__submenu-entries">
-                        <ul class="nav__submenu-entries-list">
-                            <?= $nav_entries ?>
-                            <li class="nav__submenu-entries-list-tel"><span class='nav__tel-txt'>Say hello - </span>0800 112 3228</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
-</div>
+<nav class="nav">
+    <ul class="nav__elements">
+        <li class="nav__element nav__home"><a href="/" class="nav__home-link"><span class="sr-only">Home</span></a></li>
+        <li class="nav__element nav__submenu">
+            <ul>
+                <li class="nav__submenu-toggle">
+                    <div class="nav__submenu-toggle-bar"></div>
+                </li>
+                <li class="nav__submenu-entries">
+                    <ul class="nav__submenu-entries-list">
+                        <?= $nav_entries ?>
+                        <li class="nav__submenu-entries-list-tel"><span class='nav__tel-txt'>Say hello - </span>0800 112 3228</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</nav>
