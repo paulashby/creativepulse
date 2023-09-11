0989892a0abcd25de3154a320935713306901edd
Paul Ashby <paul@primitive.co>
2023-09-11 13:55:17 +0100

    Consolidate custom style inputs in single field

--- src/ready.php
+++ src/ready.php
@@ -14,85 +14,47 @@ if (!defined("PROCESSWIRE"))
         }
     
         $custom_styles = "";
-        $page_bg_color = $page->getFormatted('bg_color');
-
-        if ($page_bg_color && $page_bg_color !== "#ffffff") {
-            $custom_styles .= ".nav, .nav__submenu-entries {\n\tbackground-color: $page_bg_color;\n}\n@media screen and (min-width: 650px) {\n\t.project-header {\n\tbackground-color: $page_bg_color;\n\t}\n}\n";
-        }
-        
+        $styles_out = "";
         $page_styles = $page->custom_styles;
 
         if ($page_styles) {
-            $custom_styles .= getCustomStyles($page_styles, "main");
+            $styles_out .= ".{$page->name} main {\n\t" .
+            str_replace("\n", "\n\t", $page_styles) .
+            "\n}\n";
         }
         
         $components = $page->project_component;        
     
         foreach ($components as $component) {
             $id = $component->id;
-            $parent_selector = "#component_$id.component";
 
             $component_bg_color = $component->getFormatted('bg_color');
+            $custom_color = $component_bg_color && $component_bg_color !== "#ffffff";
+            $custom_styles = $component->custom_styles;
 
-            if ($component_bg_color && $component_bg_color !== "#ffffff") {
-                $custom_styles .= "$parent_selector {\n\tbackground-color: $component_bg_color;\n}\n";
+            if ($custom_color || $custom_styles) {
+                $styles_out .= "#component_$id.component {\n";
+
+                if ($custom_color) {
+                    $styles_out .= "\tbackground-color: $component_bg_color;";
+                }
+
+                if ($custom_styles) {
+                    $styles_out .= "\n\t" .
+                    str_replace("\n", "\n\t", $custom_styles);
+                }
+                $styles_out .= "\n}\n";
             }
 
-            $component_styles = $component->custom_styles;
-            
-            if ($component_styles) {
-                $custom_styles .= getCustomStyles($component_styles, $parent_selector);
-            }
         }
-
-        $custom_styles .= "\n";
 
         $templates_dir = $this->config->paths->templates;
         $project_name = $page->name;
         $file_name = $templates_dir . "css/custom/{$project_name}.css";
-        $int = $this->files->filePutContents($file_name, $custom_styles);
+        $int = $this->files->filePutContents($file_name, $styles_out);
 
         if (!$int) {
             $this->warning("There was a problem writing custom styles to file");
         } 
 
     });
-
-    function getCustomStyles($custom_styles_repeater, $parent_selector) {
-        $entries = "";
-
-        foreach ($custom_styles_repeater as $media_query) {
-
-            $entry = [
-                "start_query_block" => "",
-                "styles" => "",
-                "end_query_block" => ""
-            ];
-                
-            // Media query feature is eg 'min-width'
-            $mq_feature = $media_query->media_query_feature_type->name;
-            $tabs = "\t";
-            $tabs_mq = "";
-
-            if ($mq_feature !== "base") {
-                // Not base styles, so need to be wrapped in a media query
-                $mq_width = $media_query->media_query_width;
-                $entry["start_query_block"] = "@media screen and ($mq_feature: {$mq_width}px) {\n";
-                $entry["end_query_block"] = "}\n";
-                $tabs = "\t\t";
-                $tabs_mq = "\t";
-            }
-
-            if ($media_query->styles) {
-                foreach ($media_query->styles as $style) {
-                    $selector =  $style->selector;
-                    $rules = $style->rules;
-                    $rules_tabbed = $tabs . str_replace("\n", "\n$tabs", $rules);
-
-                    $entry["styles"] .= "{$tabs_mq}$parent_selector $selector {\n$rules_tabbed\n$tabs_mq}\n";
-                }
-            }
-            $entries .= implode("", $entry);
-        }
-        return $entries;
-    }
