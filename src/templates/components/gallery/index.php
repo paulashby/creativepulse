e96fa73833e9fbc5b0dd165f3119cc0a15ce7d4c
Paul Ashby <paul@primitive.co>
2023-09-16 13:12:03 +0100

    Use placeholder image in gallery

--- src/templates/components/gallery/index.php
+++ src/templates/components/gallery/index.php
@@ -1,13 +1,14 @@
 <?php namespace ProcessWire;
 
 define("BLOCK_COUNT", 12);
+$placeholder_image = $pages->get(1050)->gallery_image->first();
 
 ?>
 
 <ul class="gallery__blocks">
     <?php for ($i = 0; $i < BLOCK_COUNT; $i++) : ?>
         <li id="block--<?= $i ?>" class="gallery__block">
-            <div class="gs_reveal_img gallery__image"></div>
+            <img src="<?= $placeholder_image->url ?>"  alt="<?= $placeholder_image->description ?>" class="gs_reveal_img gallery__image">
             <button class="bttn-icon bttn-icon--info-on"><span class="bttn__text bttn__text--hidden">More information</span></button> 
             <div class="gallery__info">
                 <button class="bttn-icon bttn-icon--info-off"><span class="bttn__text bttn__text--hidden">Close</span></button>
