4050326b0f04a3149edf1527cc1a44adccb8e9ef
Paul Ashby <paul@primitive.co>
2023-10-30 16:26:37 +0000

    Adjust styles for project components

--- src/templates/components/testimonial/index.php
+++ src/templates/components/testimonial/index.php
@@ -10,11 +10,13 @@ $attribution_role = $component->attribution_role;
 
 <div class="gs_reveal component-content">
     <h2 class="subheading"><?= $heading ?></h2>
-    <div class="text">
-        <?= $text ?>
-    </div>
-    <div class="attribution">
-        <p class="attribution_name"><?= $attribution_name ?></p>
-        <p class="attribution_role"><?= $attribution_role ?></p>
+    <div class="detail">
+        <div class="text">
+            <?= $text ?>
+        </div>
+        <div class="attribution">
+            <p class="attribution_name"><?= $attribution_name ?></p>
+            <p class="attribution_role"><?= $attribution_role ?></p>
+        </div>
     </div>
 </div>
