bf24e44a1f9efae92d73b596e68a7e787a85284d
Paul Ashby <paul@primitive.co>
2024-03-29 19:23:51 +0000

    Mute video component to allow auto play

--- src/templates/components/video/index.php
+++ src/templates/components/video/index.php
@@ -5,7 +5,7 @@ $video_url = $component->video->url;
 ?>
 
 <div class="gs_reveal component-content">
-    <video autoplay="" controls="" controlslist="" loop="" preload="metadata">
+    <video autoplay="" muted="" controls="" controlslist="" loop="" preload="metadata">
         <source type="video/mp4" src="<?= $video_url ?>">
     </video>
 </div>
