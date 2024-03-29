a7b20bee9caa71191870a18043bcf8d6fbc51977
Paul Ashby <paul@primitive.co>
2024-03-29 14:24:14 +0000

    Allow heroes without images

--- src/templates/project.php
+++ src/templates/project.php
@@ -5,28 +5,35 @@
 $display_title = $page->display_title;
 $intro = $page->intro;
 $scope = $page->scope;
+$hero_markup = "";
 
-$hero_img_options = [
-    "class" => "hero-image",
-    "lazy_load" => false,
-    "webp" => true
-];
+if (count($page->hero_image)) {
+    $hero_img_options = [
+        "class" => "hero-image",
+        "lazy_load" => false,
+        "webp" => true
+    ];
 
-$hero = $page->hero_image->first();
-$dsc = $hero->description;
-$hero_img_options["alt_str"] = $dsc ? $dsc : "Hero image capturing the essence of the {$page->title} project";
-$hero_img_options["image"] = [
-    "hero_image" => [
-        "image" => $hero,
-        "media" => "(min-width: 650px)",
-        "sizes" => "(min-width: 1200px) 1130px, (min-width: 1040px) 80vw, (min-width: 660px) 100vw"
-    ],
-    "hero_image_narrow" => [
-        "image" => $page->hero_image_narrow->first(),
-        "media" => "(max-width: 649px)",
-        "sizes" => "100vw"
-    ]
-];
+    $hero = $page->hero_image->first();
+    $dsc = $hero->description;
+    $hero_img_options["alt_str"] = $dsc ? $dsc : "Hero image capturing the essence of the {$page->title} project";
+    $hero_img_options["image"] = [
+        "hero_image" => [
+            "image" => $hero,
+            "media" => "(min-width: 650px)",
+            "sizes" => "(min-width: 1200px) 1130px, (min-width: 1040px) 80vw, (min-width: 660px) 100vw"
+        ],
+        "hero_image_narrow" => [
+            "image" => $page->hero_image_narrow->first(),
+            "media" => "(max-width: 649px)",
+            "sizes" => "100vw"
+        ]
+    ];
+    $lazy_hero_img = getLazyImageMarkup($hero_img_options);
+    $hero_markup = "<div class='gs_reveal hero'>
+        $lazy_hero_img
+    </div>";
+}
 
 $components = $page->project_component;
 $component_markup = [];
@@ -56,11 +63,8 @@ foreach ($components as $component) {
                 <h2 class="head--client"><?= $title ?></h2>
                 <p class="scope"><?= $scope ?></p>
             </div>
-            <div class="gs_reveal hero">
-                <?= getLazyImageMarkup($hero_img_options) ?>
-            </div>
+            <?= $hero_markup ?>
         </div>
     </div>
-
 	<?= implode($component_markup) ?>
 </main>
