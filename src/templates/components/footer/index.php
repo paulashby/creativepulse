104c4f316e7ac85d8be039a2dc786cb8d4921564
Paul Ashby <paul@primitive.co>
2024-04-10 13:14:31 +0100

    Handle form submission. Add validation and status.

--- src/templates/components/footer/index.php
+++ src/templates/components/footer/index.php
@@ -1,36 +1,50 @@
+<?php namespace ProcessWire;
+
+    $form_action = $pages->get("template=utilities-ajax")->url;
+
+?>
+
 <footer class="gs_reveal">
     <div class="footer__text">
         <h2>Have a project <span class="no-wrap">in mind?</span></h2>
         <p>Please call, email, or fill in this form to let us know <span class='no-wrap'>what you&apos;re looking for.</span></p>
     </div>
-    <form class="contact-form" action="/">
+    <form id="cf" class="contact-form" action="<?= $form_action ?>">
         <fieldset class="details">
+            <label for="name" class="form-label sr-only prevention"></label>
+            <input name="name" type="text" id="name" class="prevention" autocomplete="off">
             <label for="fname" class="form-label sr-only">First name</label>
-            <input type="text" id="fname" name="fname" placeholder="First name">
+            <input type="text" id="fname" name="fname" placeholder="First name" pattern="[A-Za-z]+" required>
             <label for="lname" class="form-label sr-only">Last name</label>
-            <input type="text" id="lname" name="lname" placeholder="Last name">
+            <input type="text" id="lname" name="lname" placeholder="Last name" pattern="[A-Za-z]+" required>
             <label for="email" class="form-label sr-only">Email</label>
-            <input type="email" id="email" name="email" placeholder="Email">
+            <input type="email" id="email" name="email" placeholder="Email" pattern="^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$" required>
             <label for="subject" class="form-label sr-only">Subject</label>
-            <input type="text" id="subject" name="subject" placeholder="Subject">
+            <input type="text" id="subject" name="subject" placeholder="Subject" pattern="^([a-zA-Z0-9 _\-!?'`,.]+)$" required>
         </fieldset>
         <label for="message" class="form-label sr-only">Message</label>
-        <textarea id="message" name="message" placeholder="Message"></textarea>
+        <textarea id="message" name="message" placeholder="Message" required></textarea>
         <fieldset class="consent-set">
-            <input type="checkbox" id="consent" class="consent-checkbox" name="consent" value="consent">
+            <input type="checkbox" id="consent" class="consent-checkbox" name="consent" value="consent" required>
             <label for="consent" class="consent-label">I consent to my information being stored until this <span class='no-wrap'>enquiry is resolved</span></label>
         </fieldset>
         <input type="submit" class="bttn-cp bttn-cp--form" value="send">
+        <div class="form-status">
+            <p>Thanks for contacting us - we'll get back to you really soon!</p>
+            <div class="spinner">
+                <div class="lds-dual-ring"></div>
+            </div>
+        </div>
     </form>
     <address>
         The Creative Pulse<br>
         2 West Street, Epsom,<br>
         Surrey KT18 7RG
-    </address>    
+    </address>
     <p class="contact">0800 112 3228</p>
     <p class="copyright">&copy; 2023 The Creative Pulse</p>
     <ul class="footer__links">
-        <li><a href="/projects/volvic">Projects</a></li>
-        <li><a href="/about">About Us</a></li>
+        <li id="footer-link__projects"><a href="/#nav-link__projects">Projects</a></li>
+        <li id="footer-link__about"><a href="/about">About Us</a></li>
     </ul>
 </footer>
--- src/templates/components/footer/script.js
+++ src/templates/components/footer/script.js
@@ -1,7 +1,77 @@
 const init = () => {
-    console.log("footer init");
+    const contactForm = document.querySelector("#cf");
+    contactForm.addEventListener("submit", handleFormSubmit);
+    setValidationMessages();
 };
+
+const setValidationMessages = () => {
+    const validations = {
+        fname: "Please enter your first name (letters only)",
+        lname: "Please enter your last name (letters only)",
+        email: "Please enter a valid email address",
+        subject: "Please enter a subject (numbers, letters, spaces, underscores and dashes only)",
+        message: "Please enter a message (numbers, letters, spaces, underscores and dashes only)"
+    };
+
+    for (const [key, value] of Object.entries(validations)) {
+        const inputField = document.getElementById(key);
+        inputField.addEventListener("input", function(e){
+            inputField.setCustomValidity("");
+        });
+        inputField.addEventListener("invalid", function(e){
+            inputField.setCustomValidity(value);
+        });
+    }
+}
+
+const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const url = "/tools/ajax/";
+    const formStatusDisplay = form.querySelector(".form-status");
+    const formStatusMessage = formStatusDisplay.querySelector("p");
+
+    formStatusDisplay.classList.add("form-status--busy");
+
+    try {
+        const formData = new FormData(form);
+		const response = await postFormData({ url, formData });
+        form.reset();
+        formStatusMessage.textContent = response.data;
+        formStatusDisplay.classList.remove("form-status--busy");
+        formStatusDisplay.classList.add("form-status--active");
+
+        const hideStatus = setTimeout(() => {
+            formStatusDisplay.classList.remove("form-status--active");
+            clearTimeout(hideStatus);
+          }, 3000);
+
+	} catch (error) {
+		console.error(error);
+	}
+
+};
+
+const postFormData = async ({ url, formData }) => {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataUrlEncoded = new URLSearchParams(plainFormData);
+    const fetchOptions = {
+		method: "POST",
+		headers: {
+			"X-Requested-With": "XMLHttpRequest"
+		},
+		body: formDataUrlEncoded,
+	};
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+		const errorMessage = await response.text();
+		throw new Error(errorMessage);
+	}
+    return response.json();
+}
 
 export const footer = {
     init: init
-  }
+}
