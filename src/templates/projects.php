5c4d4c4588405f1de29074c1205ee0c49a9f754d
Paul Ashby <paul@primitive.co>
2024-04-10 13:21:44 +0100

    Populate projects page - this page may not be required

--- src/templates/projects.php
+++ src/templates/projects.php
@@ -1,3 +1,17 @@
 <?php namespace ProcessWire;
 
-/* Projects template */
+// Document title
+$projects = $pages->find("template=project");
+$gallery_options = [
+    "type" => "gallery",
+    "width" => "page-width",
+    "content" => $projects
+];
+
+$gallery = renderComponent($gallery_options);
+
+?>
+
+<main data-pw-id="main-region">
+    <?= $gallery ?>
+</main>
