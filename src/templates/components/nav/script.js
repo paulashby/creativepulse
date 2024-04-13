cea35885130d3d343f814796d181eefcb23b7185
Paul Ashby <paul@primitive.co>
2024-04-13 16:48:22 +0100

    Remove projects gallery event handler as this is now in main

--- src/templates/components/nav/script.js
+++ src/templates/components/nav/script.js
@@ -5,19 +5,6 @@ const init = () => {
     toggle.addEventListener("click", () => {
         body.classList.toggle("nav-active");
     });
-
-    if (body.classList.contains("home")) {
-        // Scroll to projects gallery when 'Projects' link clicked
-        const projectsLink = body.querySelector("#nav-link__projects");
-        const projectsGallery = body.querySelector("#project-gallery");
-
-        projectsLink.addEventListener("click", (e) => {
-            const scrollTarget = projectsGallery;
-            scrollTarget.scrollIntoView({behavior: "smooth"});
-            e.preventDefault();
-        });
-    }
-
 };
 
 export const nav = {
