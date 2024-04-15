05a196ce3b06a18b452339d0ce7e79b93a558cda
Paul Ashby <paul@primitive.co>
2024-04-15 20:43:44 +0100

    Add image to 404 page

--- src/templates/basic-page.php
+++ src/templates/basic-page.php
@@ -1,11 +1,21 @@
-<?php namespace ProcessWire; 
+<?php namespace ProcessWire;
 
-// Template file for pages using the “basic-page” template
+$options = [
+    "component" => $page
+];
+
+$component_inner = wire("files")->render("components/image-full-width/index.php", $options);
 
 ?>
 
-
-<div id="content">
-	Basic page content 
-</div>	
+<main data-pw-id="main-region">
+    <div class="component component--text-width">
+        <div class="component-content">
+            <h1><?= $page->heading ?></h1>
+        </div>
+    </div>
+    <div class="component image-full-width component--page-width">
+        <?= $component_inner ?>
+    </div>
+</main>
 
