8ab6fb68a529941fbfacad5adedbf0a21f9eb1ac
Paul Ashby <paul@primitive.co>
2024-04-24 15:19:23 +0100

    Show all projects in nav menu for mobile devices

--- src/templates/components/nav/index.php
+++ src/templates/components/nav/index.php
@@ -1,8 +1,26 @@
 <?php namespace ProcessWire;
 
-// We're now only including Projects in the navigation - this with the #nav-link__projects id appended
+// We're now including only Projects in the navigation - this with the #nav-link__projects id appended
 // So the Projects link goes to the gallery on the home page while the logo home page link just loads the page
-$nav_entries = "<li class='nav__submenu-entries-list-link link__projects'><a href='/#project-gallery'>Projects</a></li>";
+
+$projects = $pages->find("template=project");
+// Generate navigation list of projects
+$project_links = $projects->each(function($project) {
+    return "<li class='nav__submenu-entries-list-link'><a href='$project->url'>$project->title</a></li>";
+});
+
+$nav_entries = "<li class='nav__submenu-entries-list-link link__projects'>
+    <a href='/#project-gallery' >Projects</a>
+</li>
+<li class='nav__submenu-projects-list'>
+    <ul>
+        $project_links
+    </ul>
+</li>";
+
+
+
+
 
 ?>
 
